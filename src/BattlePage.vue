e80a2760938d1daad2dfcf54e28ee34f0b6b7d23
emred007 <emreduman701@gmail.com>
2024-11-14 15:19:06 +0200

    Revert "Update BattlePage.vue"
    This reverts commit 7790e1982f9099511b50a62596693d73ac129ecc.

--- src/BattlePage.vue
+++ src/BattlePage.vue
@@ -13,7 +13,7 @@
 
 
 <section name="Canavarin HTML'i" class="mt-16 mx-14">
-<p>Canavarımsı</p>    
+<p>Canavar</p>    
 <v-progress-linear
   v-model="canavar.can"
   height="40" 
@@ -39,7 +39,7 @@ var oyuncu {
     kalkan:500,
 }
 var canavar {
-    can:110,
+    can:100,
     saldiriGücü:300,
     kalkan:600,
 }
