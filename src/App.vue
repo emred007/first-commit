0cb2c384abc56f99ff8cc31952a02435a57cf69e
emred007 <emreduman701@gmail.com>
2024-11-14 14:58:57 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-app>
+    <v-main>
+      <BattlePage></BattlePage>
+    </v-main>
+
+  </v-app>
+</template>
+
+<script setup>
+import BattlePage from './BattlePage.vue';
+
+  //
+</script>
---
+++ src/BattlePage.vue
@@ -0,0 +1,50 @@
+<template>
+ <h1 class="bg-purple text-center mx-10 my-15 py-10">VERSUS</h1>
+
+<section name="Oyuncunun HTML'i" class="mt-16 mx-14">
+<p>Oyuncu</p>
+<v-progress-linear
+  v-model="oyuncu.can"
+  height="40" 
+  color="green"
+></v-progress-linear>
+</section>
+
+
+
+<section name="Canavarin HTML'i" class="mt-16 mx-14">
+<p>Canavar</p>    
+<v-progress-linear
+  v-model="canavar.can"
+  height="40" 
+  color="green"
+></v-progress-linear>
+</section>
+
+
+<section name="Tuşlar" class="text-center mt-7 mx-14">
+    <v-btn class="bg-purple mt-1" block size="large">Saldir</v-btn>
+    <v-btn class="bg-purple mt-1" block size="large">Pot Bas</v-btn>    
+    <v-btn class="bg-purple mt-1" block size="large">Özel Atak Yap</v-btn>
+</section>
+
+</template>
+
+<script setup>
+import (ref) from "vue"
+
+var oyuncu {
+    can:100,
+    saldiriGücü:250,
+    kalkan:500,
+}
+var canavar {
+    can:100,
+    saldiriGücü:300,
+    kalkan:600,
+}
+</script>
+
+<style scoped>
+
+</style>
